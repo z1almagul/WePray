1f281c4c49b523da4757373228f94995dc3d67fd
Zeynep Aydin <78844470+z1almagul@users.noreply.github.com>
2024-11-01 20:49:06 +0100

    Update script.js
    adding keydwn

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ button.addEventListener('click', e => {
         image.style.display = "block";
     }
 });
+
+button.addEventListener('keydown', function(e){
+    if(e.key === 'Enter'){
+        var userName = document.getElementById('name').value;
+    var image;
+    this.images = document.getElementsByClassName('image');
+    this.message = document.getElementById('message');
+    
+    if(userName==''){
+       for (let i = 0; i < images.length; i++)
+            images[i].style.display = "none";
+        this.message = 'No image found for the corresponding name.';
+    }else{
+        this.message = '';
+        const imgName = userName.toLowerCase();
+        let image = document.getElementById(imgName);
+        /*scale image
+        let imageWidth = image.width;
+        let imageHeight = image.height;
+        let scaleFactor = Math.min(canvas.width / image.width , canvas.height / image.height);
+        let newWidth = image.width *scaleFactor;
+        let newHeight = image.height *scaleFactor;*/
+        for (let i = 0; i < images.length; i++)
+            images[i].style.display = "none";
+        image.style.display = "block";
+    }
+    }
+});
